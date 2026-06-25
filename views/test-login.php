111d94b10e027b4acc2e4c6d18c525adde6b9354
Evan Langlois <ekl@taro3.local>
2026-06-25 06:34:51 -0500

    Simplify test view: inputs use built-in label attribute

--- views/test-login.php
+++ views/test-login.php
@@ -1,7 +1,5 @@
 <form>
-    <label>Username</label>
-    <input name="username" type="text" placeholder="username">
-    <label>Password</label>
-    <input name="password" type="password" placeholder="password">
+    <input name="username" type="text" label="Username" placeholder="username">
+    <input name="password" type="password" label="Password" placeholder="password">
     <button name="login" hx-post="/loginform/login/login/">Login</button>
 </form>
